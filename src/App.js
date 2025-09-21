e98963a7cf685e560cd7878d8b6cd9cb43b2088b
rlacodud <rlacodud0313@naver.com>
2025-09-21 14:07:22 +0900

    refactor: counter 모듈 Action Creator 적용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+// 사용할 Action creator를 import 합니다.
+import { minusOne, plusOne } from "./redux/modules/counter";
+
 const App = () => {
   const dispatch = useDispatch();
   const number = useSelector((state) => state.counter.number);
@@ -11,14 +14,14 @@ const App = () => {
       {number}
       <button
         onClick={() => {
-          dispatch({ type: "PLUS_ONE" });
+          dispatch(plusOne()); // 액션객체를 Action creator로 변경합니다.
         }}
       >
         + 1
       </button>
       <button
         onClick={() => {
-          dispatch({ type: "MINUS_ONE" });
+          dispatch(minusOne()); // 액션객체를 Action creator로 변경합니다.
         }}
       >
         - 1
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,25 +1,42 @@
+// 추가된 코드 👇 - 액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
+const PLUS_ONE = "PLUS_ONE";
+const MINUS_ONE = "MINUS_ONE";
+
+
+// 추가된 코드 👇 - Action Creator를 만들어 줍니다. 
+export const plusOne = () => {
+  return {
+    type: PLUS_ONE,
+  };
+};
+
+export const minusOne = () => {
+  return {
+    type: MINUS_ONE,
+  };
+};
+
+
 // 초기 상태값
 const initialState = {
-    number: 0,
+  number: 0,
 };
 
 // 리듀서
 const counter = (state = initialState, action) => {
-    switch (action.type) {
-        case "PLUS_ONE":
-        return {
-            number: state.number + 1,
-        };
-
-        // action.type이 MINUS_ONE 일 때 새로운 state 반환
-        case "MINUS_ONE":
-        return {
-            number: state.number - 1,
-        };
-        default:
-        return state;
-    }
+  switch (action.type) {
+    case PLUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다. 
+      return {
+        number: state.number + 1,
+      };
+    case MINUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다. 
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
 };
 
-// 모듈파일에서는 리듀서를 export default 한다.
+
 export default counter;
