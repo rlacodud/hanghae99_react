e3e0bd4626e04501d7bb58747d6a6224c3e3d7b8
rlacodud <rlacodud0313@naver.com>
2025-09-21 14:20:22 +0900

    feat: addNumber payload 추가

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-
 import React from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-// 사용할 Action creator를 import 합니다.
-import { minusOne, plusOne } from "./redux/modules/counter";
+// 4. Action Creator를 import 합니다.
+import { addNumber } from "./redux/modules/counter";
 
 const App = () => {
+	// 1. dispatch를 사용하기 위해 선언해줍니다.
   const dispatch = useDispatch();
-  const number = useSelector((state) => state.counter.number);
+  const [number, setNumber] = useState(0);
+  const globalNumber = useSelector((state) => state.counter.number);
+
+  const onChangeHandler = (event) => {
+    const { value } = event.target;
+    setNumber(+value);
+  };
+
+	// 2. 더하기 버튼을 눌렀을 때 실행할 이벤트핸들러를 만들어줍니다.
+  const onClickAddNumberHandler = () => {
+		// 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
+    dispatch(addNumber(number));
+  };
 
   return (
     <div>
-      {number}
-      <button
-        onClick={() => {
-          dispatch(plusOne()); // 액션객체를 Action creator로 변경합니다.
-        }}
-      >
-        + 1
-      </button>
-      <button
-        onClick={() => {
-          dispatch(minusOne()); // 액션객체를 Action creator로 변경합니다.
-        }}
-      >
-        - 1
-      </button>
+      <div>{globalNumber}</div>
+      <input type="number" onChange={onChangeHandler} />
+			{/* 3. 더하기 버튼 이벤트핸들러를 연결해줍니다. */}
+      <button onClick={onClickAddNumberHandler}>더하기</button>
+      <button>빼기</button>
     </div>
   );
 };
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,42 +1,33 @@
-// 추가된 코드 👇 - 액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
-const PLUS_ONE = "PLUS_ONE";
-const MINUS_ONE = "MINUS_ONE";
+// src/redux/modules/counter.js
 
+// Action Value
+const ADD_NUMBER = "ADD_NUMBER";
 
-// 추가된 코드 👇 - Action Creator를 만들어 줍니다. 
-export const plusOne = () => {
+// Action Creator
+export const addNumber = (payload) => {
   return {
-    type: PLUS_ONE,
+    type: ADD_NUMBER,
+    payload,
   };
 };
 
-export const minusOne = () => {
-  return {
-    type: MINUS_ONE,
-  };
-};
-
-
-// 초기 상태값
+// Initial State
 const initialState = {
   number: 0,
 };
 
-// 리듀서
+// Reducer 기본형태
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case PLUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다. 
+    case ADD_NUMBER:
       return {
-        number: state.number + 1,
-      };
-    case MINUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다. 
-      return {
-        number: state.number - 1,
+				// state.number (기존의 nubmer)에 action.paylaod(유저가 더하길 원하는 값)을 더한다.
+        number: state.number + action.payload,
       };
     default:
       return state;
   }
 };
 
-
+// export default reducer
 export default counter;
