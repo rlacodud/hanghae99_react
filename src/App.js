762b5e1381f80930d011794cabe375dc246b2c85
rlacodud <rlacodud0313@naver.com>
2025-09-21 14:26:43 +0900

    feat: minusNumber 기능 추가(퀴즈)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // 4. Action Creator를 import 합니다.
-import { addNumber } from "./redux/modules/counter";
+import { addNumber, minusNumber } from "./redux/modules/counter";
 
 const App = () => {
 	// 1. dispatch를 사용하기 위해 선언해줍니다.
@@ -17,9 +17,17 @@ const App = () => {
   };
 
 	// 2. 더하기 버튼을 눌렀을 때 실행할 이벤트핸들러를 만들어줍니다.
-  const onClickAddNumberHandler = () => {
+  const onClickNumberHandler = (type) => {
 		// 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
-    dispatch(addNumber(number));
+    switch(type) {
+      case 'plus':
+        dispatch(addNumber(number));
+      break;
+      case 'minus':
+        dispatch(minusNumber(number));
+      break;
+      default:
+    }
   };
 
   return (
@@ -27,8 +35,8 @@ const App = () => {
       <div>{globalNumber}</div>
       <input type="number" onChange={onChangeHandler} />
 			{/* 3. 더하기 버튼 이벤트핸들러를 연결해줍니다. */}
-      <button onClick={onClickAddNumberHandler}>더하기</button>
-      <button>빼기</button>
+      <button onClick={() => onClickNumberHandler('plus')}>더하기</button>
+      <button onClick={() => onClickNumberHandler('minus')}>빼기</button>
     </div>
   );
 };
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -2,11 +2,19 @@
 
 // Action Value
 const ADD_NUMBER = "ADD_NUMBER";
+const MINUS_NUMBER = "MINUS_NUMBER";
 
 // Action Creator
 export const addNumber = (payload) => {
   return {
     type: ADD_NUMBER,
+    payload,
+  };
+};
+
+export const minusNumber = (payload) => {
+  return {
+    type: MINUS_NUMBER,
     payload,
   };
 };
@@ -23,6 +31,11 @@ const counter = (state = initialState, action) => {
       return {
 				// state.number (기존의 nubmer)에 action.paylaod(유저가 더하길 원하는 값)을 더한다.
         number: state.number + action.payload,
+      };
+    case MINUS_NUMBER:
+      return {
+        // state.number (기존의 nubmer)에 action.paylaod(유저가 빼길 원하는 값)을 더한다.
+        number: state.number - action.payload,
       };
     default:
       return state;
